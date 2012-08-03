5bbc2605d63e823c4697dc9448f38d67eef25371
Mario Fernandez <mario.fernandez@hceris.com>
2012-08-03 23:56:13 +0200

    CCI: Search & Sort

--- src/main/java/com/hceris/sort/Search.java
+++ src/main/java/com/hceris/sort/Search.java
@@ -22,4 +22,89 @@ public class Search {
 
         return -1;
     }
+
+    public static <T extends Comparable<? super T>> int binarySearch(T[] a, T elem, int left, int right) {
+        if(left > right) {
+            return -1;
+        }
+
+        int middle = (left + right) >>> 1;
+        int cmp = elem.compareTo(a[middle]);
+
+        if(cmp == 0) {
+            return middle;
+        } else if (cmp < 0) {
+            return binarySearch(a, elem, left, middle - 1);
+        } else {
+            return binarySearch(a, elem, middle + 1, right);
+        }
+    }
+
+    public static <T extends Comparable<? super T>> int rotatedSearch(T[] a, T elem) {
+        return rotatedSearch(a, elem, 0, a.length - 1);
+    }
+
+    public static <T extends Comparable<? super T>> int rotatedSearch(T[] a, T elem, int left, int right) {
+        if(left > right) {
+            return -1;
+        }
+
+        int middle = (left + right) >>> 1;
+        int cmp = elem.compareTo(a[middle]);
+
+        if(cmp == 0) {
+            return middle;
+        }
+
+        int left_res = -1;
+        if(isSorted(a, left, middle - 1)) {
+            if(cmp < 0) {
+                left_res = binarySearch(a, elem, left, middle - 1);
+            }
+        } else {
+            left_res = rotatedSearch(a, elem, left, middle - 1);
+        }
+
+        if(left_res != -1) {
+            return left_res;
+        }
+
+        int right_res = -1;
+        if(isSorted(a, middle - 1, right)) {
+            if(cmp > 0) {
+                right_res = binarySearch(a, elem, middle + 1, right);
+            }
+        } else {
+            right_res = rotatedSearch(a, elem, middle + 1, right);
+        }
+
+        return right_res;
+    }
+
+    private static <T extends Comparable<? super T>> boolean isSorted(T[] a, int left, int right) {
+        return a[left].compareTo(a[right]) <= 0;
+    }
+
+    public static int binarySearchWithEmpty(String[] a, String elem) {
+        return binarySearchWithEmpty(a, elem, 0, a.length - 1);
+    }
+            
+    public static int binarySearchWithEmpty(String[] a, String elem, int left, int right) {
+        if(left > right) { return -1; }
+
+        int middle = (left + right) >>> 1;
+        while(middle >= 0 && a[middle].isEmpty()) {
+            middle--;
+        }
+
+        int cmp = elem.compareTo(a[middle]);
+
+        if(cmp == 0) {
+            return middle;
+        } else if (cmp < 0) {
+            return binarySearchWithEmpty(a, elem, left, middle - 1);
+        } else {
+            return binarySearchWithEmpty(a, elem, Math.max(middle, (left + right) >>> 1) + 1, right);
+        }
+    }
 }
--- src/test/java/com/hceris/sort/SearchTest.java
+++ src/test/java/com/hceris/sort/SearchTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 public class SearchTest {
 	
 	Integer[] sorted = new Integer[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	Integer[] rotated = new Integer[] { 15, 16, 19, 20, 25, 1, 3, 4, 5, 7, 10, 14};
 	
 	@Test
 	public void testBinarySearchLeftOverflow() {
@@ -27,5 +28,20 @@ public class SearchTest {
 	@Test
 	public void testBinarySearchLimit() {
 		assertEquals(9, Search.binarySearch(sorted, 9));
+	}
+	
+	@Test
+	public void testRotatedSearch() throws Exception {
+		assertEquals(8, Search.rotatedSearch(rotated, 5));
+	}
+	
+	@Test
+	public void testRotatedSearchNotThere() throws Exception {
+		assertEquals(-1, Search.rotatedSearch(rotated, 6));
+	}
+	
+	@Test
+	public void testBinarySearchWithEmpty() throws Exception {
+		assertEquals(4, Search.binarySearchWithEmpty(new String[] { "at", "", "", "", "ball", "", "", "car", "", "", "dad", "", ""}, "ball"));		
 	}
 }
