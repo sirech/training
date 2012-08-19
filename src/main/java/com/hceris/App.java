82b4f92c478c6a2e8cb00e9ae6d17769554be123
Mario Fernandez <mario.fernandez@hceris.com>
2012-08-19 21:41:47 +0200

    Make suffix tree more efficient by avoiding creating multiple substrings

--- src/main/java/com/hceris/App.java
+++ src/main/java/com/hceris/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-    	int[][] m = new int[3][2];
+//    	int[][] m = new int[3][2];
     	int[] a = new int[4];
     	a[1]--;
         System.out.println(a[1]);
--- src/main/java/com/hceris/datastructures/SuffixTree.java
+++ src/main/java/com/hceris/datastructures/SuffixTree.java
@@ -8,23 +8,23 @@ public class SuffixTree {
     public SuffixTree(String s) {
         root = new Node();
         for(int i = 0; i < s.length(); i++) {
-        	root.insert(s.substring(i));
+        	root.insert(s, i);
         }
     }
 
     public boolean contains(String s) {
-        return root.contains(s);
+        return root.contains(s, 0);
     }
 
     private static class Node {
         Map<Character, Node> nodes = new HashMap<Character,Node>(0);
 
-        public void insert(String s) {
-            if(s == null || s.isEmpty()) {
+        public void insert(String s, int current) {
+            if(s == null || current >= s.length()) {
                 return;
             }
 
-            char first = s.charAt(0);
+            char first = s.charAt(current);
             Node next = nodes.get(first);
 
             if(next == null) {
@@ -32,20 +32,20 @@ public class SuffixTree {
                 nodes.put(first, next);
             }
 
-            next.insert(s.substring(1));
+            next.insert(s, current + 1);
         }
 
-        public boolean contains(String s) {
-            if(s.isEmpty()) {
+        public boolean contains(String s, int current) {
+            if(s.isEmpty() || current >= s.length()) {
                 return true;
             }
 
-            Node next = nodes.get(s.charAt(0));
+            Node next = nodes.get(s.charAt(current));
             if(next == null) {
                 return false;
             }
 
-            return next.contains(s.substring(1));
+            return next.contains(s, current + 1);
         }
     }
 }
